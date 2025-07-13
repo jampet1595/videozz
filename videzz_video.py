e20b40135f05fdee78d4cac8fac33c78656ccf3f
jampet1595 <95120503+jampet1595@users.noreply.github.com>
2025-07-13 12:00:46 +0700

    Update videzz_video.py

--- videzz_video.py
+++ videzz_video.py
@@ -55,7 +55,7 @@ def random_mouse_move(driver):
 import requests
 
 # URL chứa file .txt
-url = "https://raw.githubusercontent.com/talblubClouby96/videzz_video/refs/heads/main/links.txt"
+url = "https://raw.githubusercontent.com/anisidina29/earn/refs/heads/main/videzzz_link.txt"
 
 # Tải nội dung từ URL
 response = requests.get(url)
